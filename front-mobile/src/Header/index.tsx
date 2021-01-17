274586829d25cbb088de6d713c6611f1a3a9c789
isaquebrother90 <improgramcode@gmail.com>
2021-01-17 15:42:14 -0300

    Add order details page and and integration with the Google Maps app

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -4,19 +4,19 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-function Header() { 
+function Header() {
   const navigation = useNavigation();
 
   const handleOnPress = () => {
-      navigation.navigate('Home');
+    navigation.navigate('Home');
   }
 
   return (
     <TouchableWithoutFeedback onPress={handleOnPress}>
-    <View style={styles.container}>
+      <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
     </TouchableWithoutFeedback>
   );
 }
